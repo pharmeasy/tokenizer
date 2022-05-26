e280bc7e6588408f2dc1d4508e0c88c6f20cb182
Nikhil Koyikkamannil <nikhil.k@pharmeasy.in>
2022-05-26 12:37:43 +0530

    appd changes

--- internal/apm/appdynamics/appdynamics.go
+++ internal/apm/appdynamics/appdynamics.go
@@ -34,35 +34,33 @@ func InitAppDynamics(cfg *config.TokenizerConfig) {
 	// init the SDK
 	if err := appdynamics.InitSDK(&appDconfig); err != nil {
 		fmt.Printf("Error initializing the AppDynamics SDK\n")
+		fmt.Printf(err.Error())
 	} else {
 		fmt.Printf("Initialized AppDynamics SDK successfully\n")
+
+		logrus.Info("Initialized AppDynamics successfully\n")
+
+		// Run some BTs
+		maxBtCount := 20000
+		btCount := 0
+
+		fmt.Print("Doing something")
+		for btCount < maxBtCount {
+			// start the "Checkout" transaction
+			btHandle := appdynamics.StartBT("MyTestGolangBT", "")
+
+			// do something....
+			fmt.Print(".")
+			milliseconds := 250
+			time.Sleep(time.Duration(milliseconds) * time.Millisecond)
+
+			// end the transaction
+			appdynamics.EndBT(btHandle)
+
+		}
+		fmt.Print("\n")
+
+		// Stop/Clean up the AppD SDK.
+		appdynamics.TerminateSDK()
 	}
-
-	logrus.Info("Initializing appdynamics")
-
-	logrus.Info("Initialized AppDynamics successfully\n")
-
-	// Run some BTs
-	maxBtCount := 20000
-	btCount := 0
-
-	fmt.Print("Doing something")
-	for btCount < maxBtCount {
-		// start the "Checkout" transaction
-		btHandle := appdynamics.StartBT("MyTestGolangBT", "")
-
-		// do something....
-		fmt.Print(".")
-		milliseconds := 250
-		time.Sleep(time.Duration(milliseconds) * time.Millisecond)
-
-		// end the transaction
-		appdynamics.EndBT(btHandle)
-
-	}
-	fmt.Print("\n")
-
-	// Stop/Clean up the AppD SDK.
-	appdynamics.TerminateSDK()
-
 }
