50ff689ca34d4eb77a7ef1b7512a5bdfdf60096d
Shivam Upadhyay <shivam.upadhyay@pharmeasy.in>
2023-01-24 15:48:29 +0530

    Remove tracing handler

--- internal/cryptography/routes.go
+++ internal/cryptography/routes.go
@@ -1,15 +1,11 @@
 package cryptography
 
-import (
-	"github.com/go-chi/chi"
-	instana "github.com/instana/go-sensor"
-)
+import "github.com/go-chi/chi"
 
 // RegisterRoutes ...
 func (c *ModuleCrypto) RegisterRoutes(router chi.Router) {
-
-	router.Post("/v1/encrypt", instana.TracingHandlerFunc(c.InstanaSensor, "", c.encrypt))
-	router.Post("/v1/decrypt", instana.TracingHandlerFunc(c.InstanaSensor, "", c.decrypt))
-	router.Post("/v1/metadata", instana.TracingHandlerFunc(c.InstanaSensor, "", c.getMetaData))
-	router.Put("/v1/metadata/update", instana.TracingHandlerFunc(c.InstanaSensor, "", c.updateMetadata))
+	router.Post("/v1/encrypt", c.encrypt)
+	router.Post("/v1/decrypt", c.decrypt)
+	router.Post("/v1/metadata", c.getMetaData)
+	router.Put("/v1/metadata/update", c.updateMetadata)
 }
