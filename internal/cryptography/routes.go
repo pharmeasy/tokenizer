62b479219790e313cd72be0c53ae37e55736b276
Shivam Upadhyay <shivam.upadhyay@pharmeasy.in>
2023-01-25 16:02:49 +0530

    Update path templates

--- internal/cryptography/routes.go
+++ internal/cryptography/routes.go
@@ -8,8 +8,8 @@ import (
 // RegisterRoutes ...
 func (c *ModuleCrypto) RegisterRoutes(router chi.Router) {
 
-	router.Post("/v1/encrypt", instana.TracingHandlerFunc(c.InstanaSensor, "test-random", c.encrypt))
-	router.Post("/v1/decrypt", instana.TracingHandlerFunc(c.InstanaSensor, "test-random-2", c.decrypt))
-	router.Post("/v1/metadata", instana.TracingHandlerFunc(c.InstanaSensor, "test-random-3", c.getMetaData))
-	router.Put("/v1/metadata/update", instana.TracingHandlerFunc(c.InstanaSensor, "test-random-4", c.updateMetadata))
+	router.Post("/v1/encrypt", instana.TracingHandlerFunc(c.InstanaSensor, "v1-encrypt", c.encrypt))
+	router.Post("/v1/decrypt", instana.TracingHandlerFunc(c.InstanaSensor, "v1-decrypt", c.decrypt))
+	router.Post("/v1/metadata", instana.TracingHandlerFunc(c.InstanaSensor, "v1-get-metadata", c.getMetaData))
+	router.Put("/v1/metadata/update", instana.TracingHandlerFunc(c.InstanaSensor, "v1-update-metadata", c.updateMetadata))
 }
