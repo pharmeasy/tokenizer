f7d04d3a5eb30f6fdc6b2837328245fd5b61b3e7
Shivam Upadhyay <shivam.upadhyay@pharmeasy.in>
2023-01-25 16:18:56 +0530

    Use named handler func

--- internal/cryptography/routes.go
+++ internal/cryptography/routes.go
@@ -8,8 +8,8 @@ import (
 // RegisterRoutes ...
 func (c *ModuleCrypto) RegisterRoutes(router chi.Router) {
 
-	router.Post("/v1/encrypt", instana.TracingHandlerFunc(c.InstanaSensor, "v1-encrypt", c.encrypt))
-	router.Post("/v1/decrypt", instana.TracingHandlerFunc(c.InstanaSensor, "v1-decrypt", c.decrypt))
-	router.Post("/v1/metadata", instana.TracingHandlerFunc(c.InstanaSensor, "v1-get-metadata", c.getMetaData))
-	router.Put("/v1/metadata/update", instana.TracingHandlerFunc(c.InstanaSensor, "v1-update-metadata", c.updateMetadata))
+	router.Post("/v1/encrypt", instana.TracingNamedHandlerFunc(c.InstanaSensor, "v1_encrypt", "/v1/encrypt", c.encrypt))
+	router.Post("/v1/decrypt", instana.TracingNamedHandlerFunc(c.InstanaSensor, "v1_decrypt", "/v1/decrypt", c.decrypt))
+	router.Post("/v1/metadata", instana.TracingNamedHandlerFunc(c.InstanaSensor, "v1_metadata", "/v1/metadata", c.getMetaData))
+	router.Put("/v1/metadata/update", instana.TracingNamedHandlerFunc(c.InstanaSensor, "v1_metadata_update", "/v1/metadata/update", c.updateMetadata))
 }
